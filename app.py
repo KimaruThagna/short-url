4cb559b3e1ca4f96053020b41a6da7dc4ee4e202
Kimaru thagana <macbuntu@localhost.localdomain>
2021-07-26 20:53:05 +0300

    setup app routes

---
+++ app.py
@@ -0,0 +1,19 @@
+from flask import current_app as app
+from flask import request, redirect
+from .models import db, UrlRecord, UrlStats
+
+@app.route('/url/<shortcode>', methods=['GET'])
+def url_shortcode_redirect(shortcode):
+    # retrieve url record using shortcode
+    # redirect using main url
+    pass
+
+@app.route('/', methods=['POST'])
+def url_record(shortcode):
+    #check for shortcode if not, create and save record
+    pass
+
+@app.route('/url/<shortcode>/stats', methods=['GET'])
+def url_shortcode_stats(shortcode):
+    #retrieve stats from stats table
+    pass
