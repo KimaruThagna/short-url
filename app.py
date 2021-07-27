32cd5d0d074da8cc334e4b28ad4c9cd4a9240f8e
Kimaru thagana <macbuntu@localhost.localdomain>
2021-07-27 13:20:00 +0300

    setup GET API views

--- app.py
+++ app.py
@@ -1,13 +1,17 @@
 import random, string
 from flask import current_app as app
-from flask import request, redirect
+from flask import request, redirect, jsonify
 from .models import db, UrlRecord, UrlStats
 
 @app.route('/url/<shortcode>', methods=['GET'])
 def url_shortcode_redirect(shortcode):
     # retrieve url record using shortcode
-    # redirect using main url
-    pass
+    url_obj = UrlRecord.query.filter_by(shortcode=shortcode).first()
+    # update count
+    stats_obj = UrlStats.query.filter_by(Urlrecord=url_obj).first()
+    stats_obj.update_access_count()
+    #redirect using main url
+    return redirect(url_obj.url)
 
 @app.route('/', methods=['POST'])
 def url_record():
@@ -19,5 +23,13 @@ def url_record():
 
 @app.route('/url/<shortcode>/stats', methods=['GET'])
 def url_shortcode_stats(shortcode):
-    #retrieve stats from stats table
-    pass
+    #retrieve stats from stats and record table
+    url_obj = UrlRecord.query.filter_by(shortcode=shortcode).first()
+    stats_obj = UrlStats.query.filter_by(Urlrecord=url_obj).first()
+    stats = {
+        "registered_on": url_obj.created_on,
+        "last_accessed_on": stats_obj.updated_on,
+        "access_count": stats_obj.access_count
+    }
+    return jsonify(stats)
+    
